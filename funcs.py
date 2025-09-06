6c89f8b19443f5eac188cacbb6b702dc7f9fe838
Adrian Steyer <adrian.steyer@posteo.de>
2025-09-06 02:30:59 +0200

    feat: music controls, shutdown, lock screen

--- funcs.py
+++ funcs.py
@@ -3,6 +3,12 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
+import os
+import keyboard
+
+# ---
+# Volume
+# ---
 
 def set_master_volume(level):
     """Sets the system speaker master volume.
@@ -20,7 +26,6 @@ def set_master_volume(level):
     volume.SetMasterVolumeLevelScalar(level, None) # type: ignore
     print(f"Master volume set to {level * 100:.0f}%")
 
-
 def get_master_volume():
     """Gets the current system speaker master volume.
     
@@ -37,6 +42,33 @@ def get_master_volume():
     level = volume.GetMasterVolumeLevelScalar() # type: ignore
     print(f"Current master volume: {level * 100:.0f}%")
     return level
+
+# ---
+# Music Controls
+# ---
+
+def play_pause_music():
+    keyboard.send("play/pause media")
+
+def next_track():
+    keyboard.send("next track")
+
+def previous_track():
+    keyboard.send("previous track")
+
+
+# ---
+# General
+# ---
+
+def lock_screen():
+    os.system("rundll32.exe user32.dll,LockWorkStation")
+
+def shutdown():
+    os.system("shutdown /s /t 0")
+
+
+
 
 if __name__ == "__main__":
     current_volume = get_master_volume()  # Get current volume
---
+++ main.py
@@ -0,0 +1,47 @@
+import funcs
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/volume", methods=["GET", "POST"])
+def volume_api():
+    if flask.request.method == "GET":
+        try:
+            return flask.jsonify({"volume": funcs.get_master_volume()}), 200
+        except Exception as e:
+            return flask.jsonify({"error": str(e)}), 500
+    elif flask.request.method == "POST":
+        try:
+            funcs.set_master_volume(flask.request.json["volume"]) # type: ignore
+            return flask.jsonify({"volume": funcs.get_master_volume()}), 200
+        except Exception as e:
+            return flask.jsonify({"error": str(e)}), 500
+    
+    return flask.jsonify({"error": "Invalid request"}), 400
+
+@app.route("/music", methods=["POST"])
+def music_api():
+    action = flask.request.json["action"] # type: ignore
+    if action == "play_pause":
+        funcs.play_pause_music()
+    elif action == "next":
+        funcs.next_track()
+    elif action == "previous":
+        funcs.previous_track()
+    return flask.jsonify({"message": "Music action successful"}), 200
+
+@app.route("/shutdown", methods=["GET"])
+def shutdown_api():
+    funcs.shutdown()
+    return flask.jsonify({"message": "Shutdown successful"}), 200
+
+@app.route("/lock", methods=["GET"])
+def lock_api():
+    funcs.lock_screen()
+    return flask.jsonify({"message": "Lock successful"}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=8080)
